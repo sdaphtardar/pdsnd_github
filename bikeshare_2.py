254565f18813dc64d4e7f8c17a43576ab33664eb
Saurabh Daphtardar <saurabh.sdaphtardar@gmail.com>
2019-02-10 19:29:06 +0530

    Added description in bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,3 +1,9 @@
+"""
+    This project is coded in Python. It provides statistical analysis about bike share systems for 3 cities in United States; Chicago, New York City and Washington.
+    In addition to providing descriptive statistical values code also provides interactive experience to the user where he/she can focus on particular city.
+    Also user has an option to view raw data 5 roes at a time.
+"""
+
 import time
 import pandas as pd
 import numpy as np
