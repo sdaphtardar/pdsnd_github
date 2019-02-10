aa3e7a622b4b1ccf9a3afd6a1c86a7bad0a288b1
Saurabh Daphtardar <saurabh.sdaphtardar@gmail.com>
2019-02-10 19:30:01 +0530

    Added iteration count in description in bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,8 @@
     This project is coded in Python. It provides statistical analysis about bike share systems for 3 cities in United States; Chicago, New York City and Washington.
     In addition to providing descriptive statistical values code also provides interactive experience to the user where he/she can focus on particular city.
     Also user has an option to view raw data 5 roes at a time.
+
+    Iteration count = 2
 """
 
 import time
