bba386c8068786d33c9593d49ad11733d9518128
Saurabh Daphtardar <saurabh.sdaphtardar@gmail.com>
2019-02-10 20:03:57 +0530

    Indented doc string in function station_stats.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -125,7 +125,9 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+        Displays statistics on the most popular stations and trip.
+    """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
